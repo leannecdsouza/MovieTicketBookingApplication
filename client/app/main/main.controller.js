f941d6b40ebfac762cfd69fc0b769db44fb98d85
Leanne <leannecdsouza@users.noreply.github.com>
2018-02-16 11:14:33 +0400

    Add files via upload

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -12,15 +12,15 @@
       this.passOn = [];
 
       $scope.$on('$destroy', function() {
-        socket.unsyncUpdates('/api/moviesendpoints');
+        socket.unsyncUpdates('moviesendpoint');
       });
     }
 
     $onInit() {
-      this.$http.get('/api/moviesendpoints/search/' + 'true ')
+      this.$http.get('/api/moviesendpoints/')
         .then(response => {
           this.moviess = response.data;
-          this.socket.syncUpdates('/api/moviesendpoints', this.moviess);
+          this.socket.syncUpdates('moviesendpoints', this.moviess);
         });
     }
 
@@ -33,14 +33,14 @@
           sessionStorage.setItem('MovieDesc', this.movie.Description);
           sessionStorage.setItem('MovieID', id);
 
-          this.socket.syncUpdates('/api/moviesendpoints', this.movie);
+          this.socket.syncUpdates('moviesendpoints', this.movie);
           document.getElementById('title').innerHTML = this.movie.MovieName;
           document.getElementById('pos').src = 'https://image.tmdb.org/t/p/w500/' + this.movie.Poster;
           document.getElementById('desc').innerHTML = this.movie.Description;
           document.getElementById('dur').innerHTML = this.movie.Duration;
           $("#id").attr('value', this.movie._id);
           $("#genre").empty();
-          for (var i = 1; i < this.movie.Genre.length; i++) {
+          for (var i = 0; i < this.movie.Genre.length; i++) {
             var n = '<div class="label label-warning">' + this.movie.Genre[i].name + '</div>&nbsp;';
             $('#genre').append(n);
           }
